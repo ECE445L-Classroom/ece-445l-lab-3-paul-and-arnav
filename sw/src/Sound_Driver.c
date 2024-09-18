74a7c3a56a59386cb0407ad0f3086653aa001257
Nav1738 <arnavvats7@gmail.com>
2024-09-18 10:52:15 -0500

    Changed speaker to Port D

--- sw/src/Sound_Driver.c
+++ sw/src/Sound_Driver.c
@@ -7,12 +7,12 @@
 
 void DAC_INIT(uint16_t data) {
     SYSCTL_RCGCSSI_R |= 0x01; // activate SSI0
-    SYSCTL_RCGCGPIO_R |= 0x04;// activate port A
-    while((SYSCTL_PRGPIO_R&0x01) == 0){}; // ready?
-    GPIO_PORTA_AFSEL_R |= 0x2C;//enable alt funct on PA 2,3,5
-    GPIO_PORTA_DEN_R |= 0x2C;// configure PA2,3,5 as SSI
-    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R&0xFF0F00FF)+0x00202200;
-    GPIO_PORTA_AMSEL_R = 0; // disable analog functionality on PA
+    SYSCTL_RCGCGPIO_R |= 0x08;// activate port D
+    while((SYSCTL_PRGPIO_R&0x08) == 0){}; // ready?
+    GPIO_PORTD_AFSEL_R |= 0x2C;//enable alt funct on PD 2,3,5
+    GPIO_PORTD_DEN_R |= 0x2C;// configure PD 2,3,5 as SSI
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R&0xFF0F00FF)+0x00202200;
+    GPIO_PORTD_AMSEL_R = 0; // disable analog functionality on PD
     SSI0_CR1_R = 0x00000000; //disable SSI, master mode                               
     SSI0_CPSR_R = 0x02;
     // 8 MHz SSIClk
